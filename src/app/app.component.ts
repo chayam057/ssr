e8f9e10d7fd552d6b486a3e3122cfb1c31c785df
Chaya Shmuz <chayash@MatrixGlobal.Com>
2018-11-11 12:30:06 +0200

    add save in session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent{
- 
-  title = 'Tour of Heroes';
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['he']);
-    translate.setDefaultLang('he');
-    translate.use('he');
-  }
+export class AppComponent implements OnInit{
+title = 'Tour of Heroes';
+constructor(public translate: TranslateService, @Inject(PLATFORM_ID) private platformId: Object) {
+translate.addLangs(['he']);
+translate.setDefaultLang('he');
+translate.use('he');
+}
+ngOnInit() {
+if (this.platformId === 'browser') {
+sessionStorage.setItem('mykey', 'sessionStorage working');
+}
+}
 }
